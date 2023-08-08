1b4e2bde355c670c057f25584a58398e0d943f54
ArBab <135508050+ArBabYan@users.noreply.github.com>
2023-08-08 22:56:10 +0400

    Add files via upload

---
+++ Map.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+// Global function to populate a map with country names and capitals
+void CountryMap(std::map<std::string, std::string>& countryMap) {
+    countryMap.insert(std::make_pair("Armenia", "Yerevan"));
+    countryMap.insert(std::make_pair("Australia", "Canberra")); 
+    countryMap.insert(std::make_pair("Brazil", "Brasilia"));
+    countryMap.insert(std::make_pair("China", "Beijing"));
+    countryMap.insert(std::make_pair("France", "Paris"));
+    countryMap.insert(std::make_pair("Germany", "Berlin"));
+    countryMap.insert(std::make_pair("India", "New Delhi"));
+    countryMap.insert(std::make_pair("Japan", "Tokyo"));
+    countryMap.insert(std::make_pair("Russia", "Moscow"));
+    countryMap.insert(std::make_pair("USA", "Washington, District of Colombia"));
+
+}
+int main()
+{
+    std::map<std::string, std::string> countries;
+    CountryMap(countries);
+    for (const auto& pair : countries) {
+        std::cout << pair.first << "  " << pair.second << std::endl;
+    }
+}
